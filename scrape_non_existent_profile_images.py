736edd59eb6c814899398d2d06045999ed0d6e24
satishluintel <49825849+satishluintel@users.noreply.github.com>
2020-01-09 22:21:40 +0545

    Create scrape_non_existent_profile_images.py

---
+++ scrape_non_existent_profile_images.py
@@ -0,0 +1,39 @@
+# PYTHON3 PROGRAM TO GRAB IMAGES FROM thispersondoesnotexist.com
+# URL thispersondoesnotexist.com/image
+
+# README
+# loop is the number of image files you would want to download from the site
+# sleep is for delay, by default 1 second delay in the code, you can change the value to any time in seconds.
+
+
+#INTRODUCTION TO THE PROGRAM
+
+print("This is a program to download pictures from the website thispersondoesnotexist.com \n")
+print("Written by Satish Luintel. github -> satishluintel \n")
+
+
+# CODE STARTS HERE
+# THIS CODE DOWNLOADS 4999 IMAGES.
+
+import urllib3
+import time	
+
+http = urllib3.PoolManager()
+loop = 1 
+
+#START DOWNLOADING FILE 
+
+while (loop<5000):
+	
+  r = http.request('GET', 'https://www.thispersondoesnotexist.com/image')
+	if(r.status == 200):
+		f = open(str(loop)+".jpg", "wb")
+		f.write(r.data)
+		f.close()
+		print 'Success! File',loop,'has been downloaded !'
+	print 'Sleeping 200 millisec ...\n'
+	time.sleep(0.2)
+	
+  loop+=1
+  
+# END OF PROGRAM 
