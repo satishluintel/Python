1dba27905c11143147b76650b6126e7496ebe25a
satishluintel <49825849+satishluintel@users.noreply.github.com>
2020-01-14 22:31:44 +0545

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,15 +1,12 @@
 
 
 # PYTHON PROGRAM TO GRAB IMAGES FROM https://ph-avatars.imgix.net/
-# URL thispersondoesnotexist.com/image
-# README
 # loop is the number of image files you would want to download from the site
 
 #INTRODUCTION TO THE PROGRAM
 
-print("This is a program to download pictures from the website thispersondoesnotexist.com \n")
+print("This is a program to download profile pictures from the website producthunt.com  \n")
 print("Written by Satish Luintel. github -> satishluintel \n")
-
 
 # CODE STARTS HERE
 
@@ -18,9 +15,9 @@ import time
 
 http = urllib3.PoolManager()
 
-loop = 343434
+loop = 343434  #starting from 343434, you can start it from 0 and end the loop at n, where n is your desired number
 
-while (loop<343438):
+while (loop<343464):
 
 	URL = 'https://ph-avatars.imgix.net/'+str(loop)+'/original?auto=format&codec=mozpng&w=1632&h=1632'
 	r = http.request('GET', URL)
